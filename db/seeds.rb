f5002d93c52e1829b2c325708bd1288143d43146
児玉継兵 <javajava552000@yahoo.co.jp>
2017-01-25 09:35:41 +0000

    20170125 18:35 push DIVE09

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
-# 100.times do |n|
-#     email = Faker::Internet.email
-#     name = Faker::Name.name
-#     password = "password"
-#     User.create(email: email,
-#                 password: password,
-#                 password_confirmation: password,
-#                 name: name
-#                 )
-# end
+100.times do |n|
+    email = Faker::Internet.email
+    name = Faker::Name.name
+    password = "password"
+    User.create(email: email,
+                password: password,
+                password_confirmation: password,
+                name: name
+                )
+end
 
 n = 1
     while n <= 100
