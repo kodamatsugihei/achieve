63d8b224b4e4c0a5c1403f999e6732d5e4d16220
kodama <javajava552000@yahoo.co.jp>
2017-03-23 08:19:51 +0900

    20170323 08:19 push heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,10 +76,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
-  config.action_mailer.default_url_options = { host: 'evening-badlands-75105.herokuapp.com'}
+
+  config.action_mailer.default_url_options = { host: 'powerful-springs-36262.herokuapp.com/'}
   ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = 
+  ActionMailer::Base.smtp_settings =
   {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
@@ -89,5 +89,5 @@ Rails.application.configure do
     authentication: :plain,
     enable_starttls_auto: true
   }
-  
+
 end
