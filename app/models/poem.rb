16c4f9062cefcff4c23c82eba941dbbd01ea4c52
kodama <javajava552000@yahoo.co.jp>
2017-03-24 08:48:37 +0900

    2017082408:48 finished DIVE14_2 push github,heroku

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,5 +1,6 @@
 class Poem < ActiveResource::Base
-  # self.site = "http://localhost:3000/"
+  # self.site = "http://localhost:3001/"
   # self.site = "https://radiant-island-16776.herokuapp.com/"
-  self.site = "https://sleepy-plateau-84260.herokuapp.com/"
+  # self.site = "https://sleepy-plateau-84260.herokuapp.com/"
+  self.site = "https://salty-retreat-57049.herokuapp.com/"
 end
