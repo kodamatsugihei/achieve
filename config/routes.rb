6cf090126d91f4f8551df00244f5f217d4e95cfe
kodama <javajava552000@yahoo.co.jp>
2017-04-09 19:01:42 +0900

    2017040919:01 push

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  resources :conversations do
+    resources :messages
+  end
+
   resources :relationships, only: [:create, :destroy]
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
